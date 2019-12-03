e9490bd337bb8095ad50a5ee47c345790f9f6ef5
eklopfstein <43417850+eklopfstein@users.noreply.github.com>
2019-12-03 15:59:31 -0500

    Update AbiogeneticTestCase.cs

--- MultiThreadingWinFormsAppAsAGroupProject/FantasticBeasts/AbiogeneticTestCase.cs
+++ MultiThreadingWinFormsAppAsAGroupProject/FantasticBeasts/AbiogeneticTestCase.cs
@@ -5,47 +5,19 @@
 using System;
 using System.Threading;
 
-namespace MultiThreadingWinFormsAppAsAGroupProject
-{
+
+namespace MultiThreadingWinFormsAppAsAGroupProject {
     /// <summary>
-    /// A Fantastic Beast that will help us maintain a secure world
+    /// Test case. Do not edit. 
     /// </summary>
-    class CasualCheetah : FantasticBeast
-    {
-        public override void SayHello()
-        {
+    class AbiogeneticTestCase : FantasticBeast {
+        public override void SayHello() {
             Console.WriteLine("Hello from " + this.GetType());
         }
-        public override void RunThread()
-        {
+        public override void RunThread() {
+            Thread.Sleep(2000);
             long num = Convert.ToInt64(request);
-            response = Convert.ToString(LPF(num));
-        }
-
-        static private long LPF(long num)
-        {
-            long test = 1;
-            bool found = false;
-            for (long i = num / 2; !found && i > 1; i--)
-            {
-                if (num % i == 0 && isPrime(i))
-                {
-                    test = i;
-                    found = true;
-                }
-            }
-            return test;
-
-        }
-        static private bool isPrime(long num)
-        {
-            bool prime = true;
-            for (long i = 2; i <= num / 2; i++)
-            {
-                if (num % i == 0)
-                    prime = false;
-            }
-            return prime;
+            response = Convert.ToString(19541);
         }
     }
 }
