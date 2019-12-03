da1d369c7915025918b499842216027a867d985f
eklopfstein <43417850+eklopfstein@users.noreply.github.com>
2019-12-03 15:48:51 -0500

    Add files via upload

---
+++ MultiThreadingWinFormsAppAsAGroupProject/FantasticBeasts/CasualCheetah.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+using System.Threading;
+
+namespace MultiThreadingWinFormsAppAsAGroupProject
+{
+    /// <summary>
+    /// A Fantastic Beast that will help us maintain a secure world
+    /// </summary>
+    class CasualCheetah : FantasticBeast
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Hello from " + this.GetType());
+        }
+        public override void RunThread()
+        {
+            Thread.Sleep(2000);
+            long num = Convert.ToInt64(request);
+            response = Convert.ToString(19541);
+        }
+    }
+}
